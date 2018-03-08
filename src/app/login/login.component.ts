3a644a12364b4cdf1f4d7890f9c0193f0a8c3c98
luisse <oppeluis@gmail.com>
2018-03-07 22:25:23 -0300

    Formulario de login con ajax

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
 import { AuthService } from '../shared/guard/auth.service';
